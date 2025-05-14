562b310df08e6af5ad60ed16f72aacdd409e3bcf
Oscar <oscarfilho279@gmail.com>
2025-05-14 14:52:25 -0300

    Atualização para o deploy

--- getit/settings.py
+++ getit/settings.py
@@ -25,9 +25,9 @@ LOGOUT_REDIRECT_URL = '/'
 SECRET_KEY = 'django-insecure-en5q$d(vau^@z4)^xp$+khbtcng1pt3p-k=8cy*)4s5uywnkzq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
